10f9a72ef4cf5362499121d1bf646e30b51fa438
AndreiaTaav <andreia.tavares@outlook.com>
2013-12-18 16:19:15 -0200

    Create index.php

---
+++ texto/index.php
@@ -0,0 +1,20 @@
+<?php
+
+$x = 19;
+$y = 06;
+
+$dia = 19;
+$mes = 06;
+$ano = 2013;
+$texto = "Meu niver é ";
+
+echo $texto . $dia . "/" . $mes . "/" . $ano;
+echo "<br/>";
+echo "Meu niver é $dia/$mes/$ano";
+echo "<br/>";
+echo "$x + $y = " . ($x + $y);
+ 
+
+
+
+?>
