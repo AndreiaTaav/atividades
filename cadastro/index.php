6c947b45c1d1f54d46c7295029a6f07f700bdf7f
AndreiaTaav <andreia.tavares@outlook.com>
2014-01-07 18:27:31 -0200

    Create index.php

---
+++ cadastro/index.php
@@ -0,0 +1,21 @@
+<?php
+    require_once("menu.php");
+    
+    session_start();
+    
+    if(isset($_SESSION["cadastros"])) {
+        
+        $cadastros = $_SESSION["cadastros"];
+        
+        foreach($cadastros as $pessoa) {
+           if($pessoa != null) { 
+                echo "$pessoa <br/>";
+            }    
+        }   
+    }
+    else{
+        echo "Nao existem pessoas cadastradas";
+        
+    }
+
+?>
