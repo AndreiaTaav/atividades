e44367b82a35a61ce47d6e43980e9a77322869d2
AndreiaTaav <andreia.tavares@outlook.com>
2014-01-09 16:47:34 -0200

    Create campos_pessoa.php

---
+++ mudan-asnocadastro/campos_pessoa.php
@@ -0,0 +1,8 @@
+<label>Nome:</label>
+<input type = "text" name = "nome" />
+<br/>
+<input type="radio" name="sexo" value="m" checked="checked" />
+<label>Masculino</label>
+<input type="radio" name="sexo" value="f" />
+<label>Feminino</label>
+<br/>
