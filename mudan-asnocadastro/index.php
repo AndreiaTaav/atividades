c1f648455d2b37c0bacf30a2f7701f400a57106e
AndreiaTaav <andreia.tavares@outlook.com>
2014-01-09 16:50:16 -0200

    Create index.php

---
+++ mudan-asnocadastro/index.php
@@ -0,0 +1,25 @@
+<?php
+    require_once("menu.php");
+    
+    session_start();
+    
+    if(isset($_SESSION["cadastros"])) {
+        
+        $cadastros = $_SESSION["cadastros"];
+        
+        echo "<dl>";
+        foreach($cadastros as $pessoa) {
+           if($pessoa != null) { 
+                echo "<dt>" . $pessoa["nome"] . "</dt>";
+                echo "<dd>Sexo: " . $pessoa["sexo"] . "</dd>";
+            
+            }
+        }   
+        echo "</dl>";
+    }
+    else{
+        echo "Nao existem pessoas cadastradas.";
+        
+    }
+
+?>
