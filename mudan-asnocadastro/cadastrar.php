f06d9b795a9d5427c042813cfad0953eb49bdce3
AndreiaTaav <andreia.tavares@outlook.com>
2014-01-09 16:46:58 -0200

    Create cadastrar.php

---
+++ mudan-asnocadastro/cadastrar.php
@@ -0,0 +1,22 @@
+<?php
+    require_once("menu.php");
+    
+    session_start();
+    
+    if(!isset($_SESSION["cadastros"])) {
+        
+        $_SESSION["cadastros"] = array();
+     }
+    
+    $nome = $_REQUEST["nome"];
+    $sexo = $_REQUEST["sexo"];
+    
+    $pessoa = array();
+    $pessoa["nome"] = $nome;
+    $pessoa["sexo"] = $sexo;
+    
+    array_push($_SESSION["cadastros"], $pessoa);
+    
+    echo "Cadastro feito com sucesso.";
+
+?>
