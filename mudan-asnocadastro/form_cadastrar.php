d15eb0ebedd2ecdd4b0fd7882dece9ad38713918
AndreiaTaav <andreia.tavares@outlook.com>
2014-01-09 16:48:38 -0200

    Create form_cadastrar.php

---
+++ mudan-asnocadastro/form_cadastrar.php
@@ -0,0 +1,22 @@
+<html>
+    <head>
+        <title>Cadastro de pessoas </title>
+        
+    </head>
+    <body>
+        <?php
+            require_once("menu.php");
+        ?>
+        
+        <form action = "cadastrar.php" method = "post">
+            <?php require_once("campos_pessoa.php"); ?>
+            <input type = "submit" value = "Enviar" />    
+        
+        </form>
+    </body>
+    
+    
+    
+    
+    
+</html>
